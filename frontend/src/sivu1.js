9042f9b9d5e5b3c1a4859216022b43a61c499084
NoonaJessica <noonaposti@gmail.com>
2023-11-24 22:47:55 +0200

    lisäyksiä main/menu

--- frontend/src/sivu1.js
+++ frontend/src/sivu1.js
@@ -66,4 +66,39 @@ const checkbox = document.getElementById("checkbox")
 checkbox.addEventListener("change", () => {
   document.body.classList.toggle("dark")
 })
+/* counter to cart items*/
+ document.addEventListener("DOMContentLoaded", function () {
+      const counterInput = document.getElementById("counter");
+      const plusBtn = document.getElementById("button_add_counter");
+      const minusBtn = document.getElementById("button_remove_counter");
+
+      let itemCount = 0;
+
+      function updateCounter() {
+        counterInput.value = itemCount;
+      }
+
+      plusBtn.addEventListener("click", function () {
+        itemCount++;
+        updateCounter();
+      });
+
+      minusBtn.addEventListener("click", function () {
+        if (itemCount > 0) {
+          itemCount--;
+          updateCounter();
+        }
+      });
+    });
+	
+function jumpToMeals() {
+      var middlePosition = window.innerHeight / 4;
+      window.scrollTo(0, middlePosition);
+    }
+
+    // Jump to top burgers
+    function jumpToBurgers() {
+var burgerPosition = window.innerHeight / 1.1;
+      window.scrollTo(0, burgerPosition);
+    }
 
