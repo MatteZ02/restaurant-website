3f57ef412bedb378230aa5626cc51a6ace2a76ac
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-11-30 14:32:29 +0200

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,1 +1,10 @@
+import RestaurantApiWrapper from "./api";
 
+const restaurantApiWrapper = new RestaurantApiWrapper();
+
+async () => {
+    const menu = await restaurantApiWrapper.getMenu();
+    for (const item of menu) {
+        // TODO: display menu items
+    }
+};
