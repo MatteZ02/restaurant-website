6d3e2b994141d8480d72f2cb2b8b8c2afdd8ced9
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-12-04 16:12:21 +0200

    Update main.ts

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -2,9 +2,9 @@ import RestaurantApiWrapper from "./api";
 
 const restaurantApiWrapper = new RestaurantApiWrapper();
 
-async () => {
-    const menu = await restaurantApiWrapper.getMenu();
-    for (const item of menu) {
-        // TODO: display menu items
-    }
-};
+const params = new URLSearchParams(window.location.search);
+const login = params.get("login");
+const redirect = params.get("redirect");
+
+if (login) {
+}
