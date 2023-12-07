85c2dbd11813684ac9bb0e873f30df02a2113362
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-12-07 12:47:33 +0200

    backend error "handling"

--- src/controllers/restaurantController.ts
+++ src/controllers/restaurantController.ts
@@ -8,12 +8,13 @@ import {
     updateRestaurant,
 } from "../core/models/restaurantModel";
 import Request from "../types/Request";
+import { noop } from "../util/util";
 
 const getRestaurant = async (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) return next(new ApiError(400, "Invalid restaurant data"));
 
-    const restaurants = await getRestaurants();
+    const restaurants = await getRestaurants().catch(noop);
 
     if (!restaurants) return next(new ApiError(500, "Failed to obtain restaurants"));
     res.status(200).json(restaurants);
@@ -27,7 +28,7 @@ const postRestaurant = async (req: Request, res: Response, next: NextFunction) =
     if (req.user.level > 1) return next(new ApiError(403, "Unauthorized"));
 
     const restaurant = req.body;
-    const r = addRestaurant(restaurant);
+    const r = addRestaurant(restaurant).catch(noop);
     if (!r) return next(new ApiError(500, "Error adding restaurant"));
     res.status(201).json(r);
 };
@@ -40,7 +41,7 @@ const putRestaurant = async (req: Request, res: Response, next: NextFunction) =>
     if (req.user.level > 2) return next(new ApiError(403, "Unauthorized"));
 
     const restaurant = req.body;
-    const r = updateRestaurant(+req.params.id, restaurant);
+    const r = updateRestaurant(+req.params.id, restaurant).catch(noop);
     if (!r) return next(new ApiError(500, "Error updating restaurant"));
     res.status(200).json(r);
 };
@@ -53,7 +54,7 @@ const deleteRestaurant = async (req: Request, res: Response, next: NextFunction)
     if (req.user.level > 1) return next(new ApiError(403, "Unauthorized"));
 
     const { id } = req.params;
-    const r = deleteRestaurantById(+id);
+    const r = deleteRestaurantById(+id).catch(noop);
     if (!r) return next(new ApiError(500, "Error deleting restaurant"));
     res.status(200).json(r);
 };
