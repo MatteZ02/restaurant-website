6cf2c9f730780697abdc1e7b5fdeafc3eb6a831d
Matte <51192395+MatteZ02@users.noreply.github.com>
2023-12-14 17:44:07 +0200

    order

--- src/order.ts
+++ src/order.ts
@@ -38,31 +38,27 @@ const f = async () => {
             confirmation.innerText = orderStatusToText(order.order_status) ?? "Unknown status";
     };
 
-    const cart = await restaurantApiWrapper.getCart().catch(noop);
-    if (!cart) return;
+    const orderItems = document.getElementById("order-items");
+    if (!orderItems) return;
+    const menu = await restaurantApiWrapper.getMenu().catch(noop);
+    if (!menu) return;
 
-    const itemCount = document.getElementById("itemcounttext");
-    if (itemCount)
-        itemCount.innerText = cart.items.reduce((acc, item) => acc + item.quantity, 0) + " items";
-
-    const items = document.getElementById("order-items");
-
-    for (const item of cart.items) {
+    for (const item of order.items) {
         const itemElement = document.createElement("div");
         itemElement.classList.add("itemcart");
         const name = document.createElement("div");
         name.classList.add("addeditem");
-        name.innerText = item.item.name;
+        name.innerText = menu.find(menuItem => menuItem.id === item.id)?.name ?? "Unknown";
         itemElement.appendChild(name);
         const price = document.createElement("div");
         price.classList.add("addeditemprice");
-        price.innerText = (+item.item.price * item.quantity).toFixed(2) + " €";
+        price.innerText = menu.find(menuItem => menuItem.id === item.id)?.price ?? "Unknown";
         itemElement.appendChild(price);
         const amount = document.createElement("div");
         amount.classList.add("addeditemamount");
         amount.innerText = item.quantity + " items";
         itemElement.appendChild(amount);
-        items?.appendChild(itemElement);
+        orderItems?.appendChild(itemElement);
     }
 };
 
